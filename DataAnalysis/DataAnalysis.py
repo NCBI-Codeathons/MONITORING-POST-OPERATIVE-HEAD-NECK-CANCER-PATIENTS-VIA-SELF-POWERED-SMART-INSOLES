ec13849bd4b57ab36319bd413d2acef10f3476f9
Sanjay Ananthanarayan <ananthanarayan.sanjay@gmail.com>
2019-11-09 21:38:26 -0600

    added some code

--- DataAnalysis/DataAnalysis.py
+++ DataAnalysis/DataAnalysis.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
+from twilio.rest import Client
+
+client = Client("ACe3e83227b81823e5696955615b829feb", "180ff0ad30dc5a0ab5bcdbe00ec01b1a")
+
 with open('test_d1.csv','r') as csvfile:
     r = csv.reader(csvfile, delimiter=',')
     data = [i for i in r]
@@ -17,10 +21,18 @@ ax.plot(c1, c2)
 plt.xlabel('Time')
 plt.ylabel('Pressure')
 fig.show()
-
-if np.mean(c2) > 160:
-    print('Person needs to stay off their feet')
-elif np.mean(c2) > 60 and np.mean(c2) < 160:
-    print('Pressure on foot is normal')
-else:
-    print('Person needs to be on their feet more')
+personWeight = 150;
+rangePercentage = 0.2
+m = 0;
+reportToDoctor = "You are within healthy range";
+while m < len(c2):
+    if (c2[m] < ((rangePercentage * personWeight) - personWeight)):
+        reportToDoctor = "You have lost more weight than necessary. Contact doctor";
+    elif (c2[m] > ((rangePercentage * personWeight) + personWeight)):
+        reportToDoctor = "You have gained more weight than necessary. Contact doctor";
+    else:
+        m = m + 1;
+#client.messages.create(to="+15127390615", 
+#                       from_="+12055399840",
+#                       body=reportToDoctor)
+print(reportToDoctor)
